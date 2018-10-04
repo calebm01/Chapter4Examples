c7c4231341629c836ee6cbac8eb181a5f2e779ac
caleb.mouritsen <caleb.mouritsen@CLC-LAB209-02.TCSD.tooelesd.org>
2018-10-04 14:12:09 -0600

    Chapter 4 Examples

---
+++ src/IfAndElseStatements/IfAndElseStatements.java
@@ -0,0 +1,29 @@
+package IfAndElseStatements;
+import java.util.Scanner;
+// Caleb Mouritsen
+// if and else statements (two way if statements)
+// 10/4/18
+
+public class IfAndElseStatements {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+				
+		//constant declinations
+		final double PI = 3.14;
+		
+		
+		double area = 0;
+		
+		double radius;
+		
+		System.out.println("enter the radius of a circle");
+		radius = input.nextDouble();
+		
+		if (radius > 0);
+		
+
+	}
+
+}
